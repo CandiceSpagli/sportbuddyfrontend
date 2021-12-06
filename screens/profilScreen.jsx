611aca895b39d441eb71754df74fd5db41b16614
Candice Spagli <cspagli34@gmail.com>
2021-12-06 17:39:00 +0100

    screen settings

--- screens/profilScreen.jsx
+++ screens/profilScreen.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { View , Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
-function profilScreen() {
-    return(
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-        >
-            <Text>Page du profil</Text>
-        </View>
-    )
+function ProfilScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Page du profil</Text>
+    </View>
+  );
 }
 
-export default profilScreen
+export default ProfilScreen;
--- screens/settings.jsx
+++ screens/settings.jsx
@@ -1,31 +1,156 @@
-import React from "react";
+import React, { useState } from "react";
 
-// import RangeSlider from "react-native-range-slider";
+import { View, Text, TextInput, StyleSheet, Picker } from "react-native";
+import { Button } from "react-native-elements";
 
-import { View, Text } from "react-native";
+import DatePicker from "react-native-datepicker";
+import { ScrollView } from "react-native-gesture-handler";
 
-function settings() {
+function Settings() {
+  const [date, setDate] = useState(new Date());
+  const [selectedSport, setSelectedSport] = useState();
+
   return (
     <View>
-      <Text>Ajout d'une session</Text>
-
-      {/* <View style={{ flex: 1, flexDirection: "row" }}>
-        <RangeSlider
-          minValue={0}
-          maxValue={100}
-          tintColor={"#da0f22"}
-          handleBorderWidth={1}
-          handleBorderColor="#454d55"
-          selectedMinimum={20}
-          selectedMaximum={40}
-          style={{ flex: 1, height: 70, padding: 10, backgroundColor: "#ddd" }}
-          onChange={(data) => {
-            console.log(data);
+      <ScrollView>
+        <Text style={styles.step}> STEP 2/3</Text>
+        <Text style={styles.text}> Enter your name</Text>
+        <TextInput style={styles.input} value={"Candice"} />
+        <Text style={styles.text}> Date of birth</Text>
+        <DatePicker
+          customStyles={{ dateInput: { borderWidth: 0 } }}
+          style={styles.date}
+          date={date}
+          mode="date"
+          placeholder="select date"
+          format="DD/MM/YYYY"
+          minDate="01-01-1930"
+          maxDate="01-01-2030"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          onDateChange={(date) => {
+            setDate(date);
           }}
         />
-      </View> */}
+        <Text style={styles.text}>Gender</Text>
+        <View style={styles.button}>
+          <Button
+            titleStyle={{ color: "white" }}
+            style={styles.woman}
+            type="clear"
+            title="Woman"
+          />
+          <Button
+            titleStyle={{ color: "white" }}
+            style={styles.man}
+            type="clear"
+            title="Man"
+          />
+        </View>
+        <Text style={styles.level}>Votre niveau en : </Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={selectedSport}
+          onValueChange={(itemValue, itemIndex) => setSelectedSport(itemValue)}
+        >
+          <Picker.Item label="Fitness" value="Fitness" />
+          <Picker.Item label="Run" value="Run" />
+          <Picker.Item label="Yoga" value="Yoga" />
+        </Picker>
+        <View>
+          <Text>hello</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  text: {
+    padding: 15,
+    color: "grey",
+    marginTop: 30,
+    marginLeft: 20,
+  },
+  container: {
+    justifyContent: "space-between",
+    padding: 10,
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "white",
+  },
 
-export default settings;
+  step: {
+    marginTop: 5,
+    color: "orange",
+    flexDirection: "row",
+    marginLeft: 300,
+  },
+  input: {
+    height: 50,
+    margin: 4,
+    borderWidth: 0.5,
+    padding: 15,
+    borderRadius: 15,
+    marginLeft: 30,
+    marginRight: 30,
+    borderColor: "rgba(244, 44, 4, 0.4)",
+  },
+  date: {
+    height: 50,
+    width: 310,
+    borderWidth: 0.5,
+    borderRadius: 15,
+    marginLeft: 30,
+    marginRight: 30,
+    borderColor: "rgba(244, 44, 4, 0.4)",
+  },
+  button: {
+    marginTop: 10,
+    flexDirection: "row",
+    height: 50,
+    justifyContent: "center",
+  },
+
+  woman: {
+    color: "white",
+    backgroundColor: "#F53A15",
+    borderWidth: 0.5,
+    borderRadius: 30,
+    height: 45,
+    width: 100,
+    borderColor: "rgba(244, 44, 4, 0.4)",
+  },
+  man: {
+    color: "white",
+    backgroundColor: "#F53A15",
+    borderWidth: 0.5,
+    borderRadius: 30,
+    height: 45,
+    width: 100,
+    marginLeft: 15,
+    borderColor: "rgba(244, 44, 4, 0.4)",
+  },
+
+  level: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginLeft: 50,
+    marginTop: 15,
+    padding: 10,
+  },
+  fitness: {
+    color: "white",
+    borderColor: "rgba(244, 44, 4, 0.4)",
+    borderWidth: 0.5,
+    borderRadius: 30,
+  },
+  picker: {
+    height: 100,
+    width: 100,
+    marginBottom: 100,
+    marginTop: -120,
+    marginLeft: 250,
+  },
+});
+
+export default Settings;
