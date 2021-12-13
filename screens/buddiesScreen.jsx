55bc75ae7a50ef6672505dc9177a5c733e165705
elie <eliepro.mc@gmail.com>
2021-12-13 18:14:06 +0100

    fjrgbuyezifbudni?Z.

--- screens/buddiesScreen.jsx
+++ screens/buddiesScreen.jsx
@@ -305,12 +305,6 @@ function BuddiesScreen(props) {
         {!isInputClicked && <BuddiesListFilter />}
         {isInputClicked && <UserSearch />}
       </View>
-      {searchInput()}
-      <View style={{ alignItems: "center" }}>{plusBtn()}</View>
-      <View>
-        {!isInputClicked && <BuddiesListFilter />}
-        {isInputClicked && <UserSearch navigation={props.navigation} />}
-      </View>
       <Navbar navigation={props.navigation} />
     </>
   );
